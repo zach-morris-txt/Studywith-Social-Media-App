c8b548822f34a0ebd78b832f757548ed92b5b0c9
zach-morris-text <oneblondhuman@gmail.com>
2024-11-11 19:15:29 -0500

    Structure and style PostForm component

---
+++ social-media/components/Post-Form-Card.js
@@ -0,0 +1,45 @@
+import Card from "./Card";
+
+export default function PostFormCard() {
+    return (
+        <Card>
+            <div className="flex gap-1">
+                <div>
+                    <div className="w-12 rounded-full overflow-hidden">
+                        <img src="https://upload.wikimedia.org/wikipedia/commons/2/2c/Default_pfp.svg" alt="pfp icon" />
+                    </div>
+                </div>
+                <textarea className="grow p-3 h-14" placeholder={'Whats on your mind?'} />
+            </div>
+            <div className="flex gap-5 items-center mt-2">
+                <div>
+                    <button className="flex gap-1">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
+                        </svg>
+                        People
+                    </button>
+                </div>
+                <div>
+                    <button className="flex gap-1">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M9 6.75V15m6-6v8.25m.503 3.498 4.875-2.437c.381-.19.622-.58.622-1.006V4.82c0-.836-.88-1.38-1.628-1.006l-3.869 1.934c-.317.159-.69.159-1.006 0L9.503 3.252a1.125 1.125 0 0 0-1.006 0L3.622 5.689C3.24 5.88 3 6.27 3 6.695V19.18c0 .836.88 1.38 1.628 1.006l3.869-1.934c.317-.159.69-.159 1.006 0l4.994 2.497c.317.158.69.158 1.006 0Z" />
+                        </svg>
+                        Check In
+                    </button>
+                </div>
+                <div>
+                    <button className="flex gap-1">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M15.182 15.182a4.5 4.5 0 0 1-6.364 0M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0ZM9.75 9.75c0 .414-.168.75-.375.75S9 10.164 9 9.75 9.168 9 9.375 9s.375.336.375.75Zm-.375 0h.008v.015h-.008V9.75Zm5.625 0c0 .414-.168.75-.375.75s-.375-.336-.375-.75.168-.75.375-.75.375.336.375.75Zm-.375 0h.008v.015h-.008V9.75Z" />
+                        </svg>
+                        Mood
+                    </button>
+                </div>
+                <div className="grow text-right">
+                    <button className="bg-blue-500 text-white px-6 py-1 rounded-md">Share</button>
+                </div>
+            </div>
+        </Card>
+    )
+}
--- social-media/pages/index.js
+++ social-media/pages/index.js
@@ -1,5 +1,6 @@
 import Card from "@/components/Card";
 import NavigationCard from "@/components/Navigation-Card";
+import PostFormCard from "@/components/Post-Form-Card";
 
 
 export default function Home() {
@@ -9,7 +10,7 @@ export default function Home() {
         <NavigationCard />
       </div>
       <div className="grow">
-        <Card>Form Here</Card>
+        <PostFormCard />
         <Card>First Post Test</Card>
       </div>
     </div>
