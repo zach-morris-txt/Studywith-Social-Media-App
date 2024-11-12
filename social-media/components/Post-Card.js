30c55e07550ed33dcfbe73fee880ed0ec753c51b
zach-morris-text <oneblondhuman@gmail.com>
2024-11-12 04:44:52 -0500

    Correct clickon clickoff bug

--- social-media/components/Post-Card.js
+++ social-media/components/Post-Card.js
@@ -25,11 +25,20 @@ export default function PostCard() {
               <p className="text-gray-500 text-sm">2 hours ago</p>
             </div>
             <div>
-                <button className="text-gray-400" onClick={openDropdown()}>
+                {!dropdownOpen && (
+                    <button className="text-gray-400" onClick={openDropdown}>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 12a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM12.75 12a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM18.75 12a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
+                        </svg>
+                    </button>
+                )}
+                {dropdownOpen && (
+                    <button className="text-gray-400">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 12a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM12.75 12a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM18.75 12a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
                     </svg>
                 </button>
+                )}
                 <ClickOutHandler onClickOut={handleClickOutsideDropdown}>
                     <div className="relative">
                         {dropdownOpen && (
