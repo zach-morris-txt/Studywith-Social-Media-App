056702c24dbd3f08e70daac368d5357139a5fbb1
zach-morris-text <oneblondhuman@gmail.com>
2024-11-13 01:52:45 -0500

    Correct the root 'active' animation inconsistency2

--- social-media/components/Navigation-Card.js
+++ social-media/components/Navigation-Card.js
@@ -24,13 +24,13 @@ export default function NavigationCard() {
                     </svg>
                     Friends
                 </Link>
-                <Link href="/saved" className={nonActiveElement}>
+                <Link href="/saved" className={pathname === "/saved" ? activeElement : nonActiveElement}>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0 1 11.186 0Z" />
                     </svg>
                     Saved Posts
                 </Link>
-                <Link href="/notifications" className={nonActiveElement}>
+                <Link href="/notifications" className={pathname === "/notifications" ? activeElement : nonActiveElement}>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0" />
                     </svg>
