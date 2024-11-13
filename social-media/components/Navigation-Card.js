6bc6a86a335e74ddd37b8864ebd131d77d148401
zach-morris-text <oneblondhuman@gmail.com>
2024-11-13 01:50:49 -0500

    Correct the root 'active' animation inconsistency

--- social-media/components/Navigation-Card.js
+++ social-media/components/Navigation-Card.js
@@ -6,7 +6,7 @@ export default function NavigationCard() {
     const activeElement = 'flex gap-3 py-3 bg-blue-500 text-white -mx-10 px-10 rounded-md shadow-md shadow-gray-300';
     const nonActiveElement = 'flex gap-3 py-3 hover:bg-blue-200 -mx-4 px-2 rounded-md transition-all hover:scale-105 hover:shadow-md hover:shadow-gray-300';
     const router = useRouter();
-    const {pathname} = router;
+    const {asPath:pathname} = router;
 
     return (
         <Card>
@@ -18,25 +18,25 @@ export default function NavigationCard() {
                     </svg>
                     Home
                 </Link>
-                <Link href="" className={nonActiveElement}>
+                <Link href="/profile/friends" className={pathname === "/profile/friends" ? activeElement : nonActiveElement}>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M18 18.72a9.094 9.094 0 0 0 3.741-.479 3 3 0 0 0-4.682-2.72m.94 3.198.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0 1 12 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 0 1 6 18.719m12 0a5.971 5.971 0 0 0-.941-3.197m0 0A5.995 5.995 0 0 0 12 12.75a5.995 5.995 0 0 0-5.058 2.772m0 0a3 3 0 0 0-4.681 2.72 8.986 8.986 0 0 0 3.74.477m.94-3.197a5.971 5.971 0 0 0-.94 3.197M15 6.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm6 3a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Zm-13.5 0a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Z" />
                     </svg>
                     Friends
                 </Link>
-                <Link href="" className={nonActiveElement}>
+                <Link href="/saved" className={nonActiveElement}>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0 1 11.186 0Z" />
                     </svg>
                     Saved Posts
                 </Link>
-                <Link href="" className={nonActiveElement}>
+                <Link href="/notifications" className={nonActiveElement}>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0" />
                     </svg>
                     Notifications
                 </Link>
-                <Link href="" className={nonActiveElement}>
+                <Link href="/" className={nonActiveElement}>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0 0 13.5 3h-6a2.25 2.25 0 0 0-2.25 2.25v13.5A2.25 2.25 0 0 0 7.5 21h6a2.25 2.25 0 0 0 2.25-2.25V15M12 9l-3 3m0 0 3 3m-3-3h12.75" />
                     </svg>
